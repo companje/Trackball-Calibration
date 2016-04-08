3979b6459d8f59dcb936274da19b71deaefb73a2
Rick Companje <rick@companje.nl>
2016-04-09 00:55:12 +0200

    animated down arrow

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ public:
   vector<ofPoint> history[3];
   float trackballOrientation = 0; //45 //90;
   ofImage image;
-  float globeRadius = .44;
+  struct { float radius = .44, smoothing = 1.0; ofQuaternion rotation; } globe;
   
   struct {
     int id;
@@ -16,13 +16,14 @@ public:
   
   void setup() {
     ofBackground(0);
+    ofSetFrameRate(60);
     ofEnableNormalizedTexCoords();
     image.load("moon.jpg");
     
     //configure or detect me
     devices[0].id = 0;
-    devices[1].id = 0; //1
-    devices[2].id = 0; //2
+    devices[1].id = 0; //1;
+    devices[2].id = 0; //2;
   }
   
   void update() {
@@ -32,6 +33,8 @@ public:
       if (history[i].size()>40) history[i].erase(history[i].begin());
       devices[i].value *= 0;
     }
+    
+    //globe.rotation *= ofQuaternion(-1, ofPoint(1,0,0));
   }
   
   void draw() {
@@ -39,26 +42,49 @@ public:
     ofTranslate(ofGetWidth()/2,ofGetHeight()/2);
     ofNoFill();
     
-    //globe
+    drawGlobe();
+    drawTrackballs();
+    drawArrowAnimation();
+  }
+  
+  void applyRotation() {
+    static ofVec3f fromAxis,toAxis;
+    static float fromAngle=0,toAngle;
+    globe.rotation.getRotate(toAngle,toAxis);
+    fromAxis += (toAxis-fromAxis) * globe.smoothing;
+    fromAngle += (toAngle-fromAngle) * globe.smoothing;
+    ofRotate(fromAngle, fromAxis.x, fromAxis.y, fromAxis.z);
+  }
+  
+  void drawGlobe() {
+    ofPushMatrix();
+    applyRotation();
     ofFill();
     ofSetColor(255);
     image.bind();
     ofEnableDepthTest();
-    ofDrawSphere(scaler(globeRadius));
+    ofDrawSphere(scaler(globe.radius));
     ofDisableDepthTest();
     image.unbind();
-
+    ofPopMatrix();
+  }
+  
+  void keyPressed(int key) {
+    ofSaveFrame();
+  }
+  
+  void drawTrackballs() {
     //trackballs
     for (int i=0; i<3; i++) {
       ofPushMatrix();
       ofPushStyle();
       ofRotateZ(i/3.*360);
-      ofTranslate(0,-scaler(globeRadius));
-//      ofRotateZ(90);
+      ofTranslate(0,-scaler(globe.radius));
       ofSetColor(0);
       ofDrawEllipse(0,0,scaler(.3),scaler(.2));
       ofSetColor(255);
-      ofDrawSphere(scaler(.1/2));
+      ofFill();
+      ofDrawCircle(0,0,scaler(.1/2));
       
       //trackball motion
       for (int j=0; j<history[i].size(); j++) {
@@ -76,15 +102,29 @@ public:
       ofPopStyle();
       ofPopMatrix();
     }
-    
+  }
+  
+  void drawArrowAnimation() {
+    int h = 175;
+    for (int i=0, n=4; i<n; i++) {
+      float ii=float(i)/n;
+      ofPushMatrix();
+      float y = int(ofGetFrameNum() + ii*h) % h;
+      ofTranslate(0, y);
+      ofSetColor(255, (1-abs(2*(y/h)-1)) * 255);
+      ofNoFill();
+      ofSetLineWidth(10);
+      ofBeginShape();
+      ofVertex(-30,-30);
+      ofVertex(0,0);
+      ofVertex(30,-30);
+      ofEndShape();
+      ofPopMatrix();
+    }
   }
   
   float scaler(float s) {
     return s * ofGetHeight()/2;
-  }
-  
-  void keyPressed(int key) {
-    ofSaveFrame();
   }
   
   virtual void mouseMoved(int device, int axis, int delta) {
